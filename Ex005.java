cd4ffc61909d307849e7181ac559eab78cb76ef4
squidboss <xxx147ac@gmail.com>
2024-09-22 22:43:10 +0200

    Ex005-006

---
+++ Ex005.java
@@ -0,0 +1,11 @@
+public class Ex005 {
+
+    public static void main(String[] args) {
+
+        int a = 37;
+        int b = 11;
+
+        System.out.println("Wynik dzielenia bez reszty: " + a/b);
+    }
+
+}
---
+++ Ex006.java
@@ -0,0 +1,11 @@
+public class Ex006 {
+
+    public static void main(String[] args) {
+
+        int a = 37;
+        int b = 11;
+
+        System.out.println("Wynik reszty z dzielenia: " + a % b);
+    }
+
+}
